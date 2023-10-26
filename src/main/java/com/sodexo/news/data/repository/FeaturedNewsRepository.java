6997442669e6013ad944cf5b3d8424a4c00008fc
yorxander <yorxander@gmail.com>
2023-10-26 11:52:29 -0300

    se agrego funcionalidad para interactuar con el front

--- src/main/java/com/sodexo/news/data/repository/FeaturedNewsRepository.java
+++ src/main/java/com/sodexo/news/data/repository/FeaturedNewsRepository.java
@@ -2,7 +2,11 @@ package com.sodexo.news.data.repository;
 
 import com.sodexo.news.data.model.FeaturedNewsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
+@RepositoryRestResource(path ="FeaturedNewsEntitys", collectionResourceRel ="FeaturedNewsEntitys" )
 public interface FeaturedNewsRepository extends JpaRepository<FeaturedNewsEntity,Integer> {
 
 
--- src/main/java/com/sodexo/news/presentation/controller/FeaturedNewsController.java
+++ src/main/java/com/sodexo/news/presentation/controller/FeaturedNewsController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin(origins = "*")
 @RequestMapping("/news")
 public class FeaturedNewsController {
     private FeaturedNewsSaveUseCase FeaturedNewsSaveUseCase;
